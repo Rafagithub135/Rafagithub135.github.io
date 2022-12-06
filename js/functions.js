4c2d9aa12427101282a2986078dbc9f3184f6f5b
Ralph <mccloskey.ralph35@gmail.com>
2022-12-06 15:16:28 -0500

    added construction image and moved skills to functions page

--- js/functions.js
+++ js/functions.js
@@ -10,3 +10,9 @@ function calculateAge() {
 }
 
 calculateAge(new Date());
+
+
+let skills = ['HTML', 'CSS', 'JavaScript', 'Ajax', 'Bootstrap', 'Jquery', 'Computer Network Wiring', 'General Aircraft Mechanic', 'General Automotive Mechanic', 'General Diesel Mechanic', 'Real Estate Investor Apprentice', 'Nationally Certified Tow Operator', 'General Household Electrical Wiring', 'General Household Plumbing Work', 'General Construction']
+
+
+(skills.map(skill => `<li>${skill}</li>`));
