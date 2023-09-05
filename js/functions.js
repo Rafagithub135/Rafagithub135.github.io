2903659f79e8c4472193de596f67caee584dc75b
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-09-04 22:40:24 -0400

    added some images and working on some js to encode a message

--- js/functions.js
+++ js/functions.js
@@ -32,5 +32,31 @@
     for (let skill of skills3) {
         document.querySelector("#skills-list3").innerHTML += `<li>${skill}</li>`;
 }
-
+    var write = document.getElementById('write');
+    write.addEventListener('click', encodeInput);
+    function encodeInput() {
+        let statement = prompt('Enter the message you would like to encode.');
+        let phrase = '';
+        let randomCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
+        let keycodeCharacter = ['h', 'm', 's'];
+        for (var i = 0; i < keycodeCharacter.length; i += 1) {
+            if (keycodeCharacter[i] === 'h') {
+                keycodeCharacter[i] = '10';
+            } else if (keycodeCharacter[i] === 'm') {
+                keycodeCharacter[i] = '2';
+            } else {
+                keycodeCharacter[i] = '5';
+            }
+        }
+        let numberOfCharacters = keycodeCharacter[Math.floor(Math.random() * keycodeCharacter.length)];
+        for (var i = 0; i < statement.length; i += 1) {
+            phrase += statement[i];
+            if (i !== statement.length - 1) {
+                for (var j = 0; j < numberOfCharacters; j += 1) {
+                    phrase += randomCharacter[Math.floor(Math.random() * randomCharacter.length)];
+                }
+            }
+        }
+    }
+        return phrase;
 })();
