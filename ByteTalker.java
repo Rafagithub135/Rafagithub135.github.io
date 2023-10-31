c1c40a0910bbdc82ba4a5ce76aff928a70fd9655
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-10-30 23:45:23 -0400

    added presidential unit citation to awards and decorations after finding out my unit was awarded the ribbon

--- ByteTalker.java
+++ ByteTalker.java
@@ -11,19 +11,22 @@ public class ByteTalker {
         String message = scanner.nextLine();
         // Creates an array of random characters for the program to insert in between each character of the message.
         String[] randomCharacters = {"a", "A", "b", "B", "c", "C", "d", "D", "e", "E", "f", "F", "g", "G", "h", "H", "i", "I", "j", "J", "k", "K", "l", "L", "m", "M", "n", "N", "o", "O", "p", "P", "q", "Q", "r", "R", "s", "S", "t", "T", "u", "U", "v", "V", "w", "W", "x", "X", "y", "Y", "z", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+"};
+        // SelectorNumbers are extra numbers that will be mixed into the time code to make it harder to crack.
+        int selectorNumbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         // Creates a variable to store the current time.
         LocalDateTime currentTime = LocalDateTime.now();
         StringBuilder emessage = new StringBuilder();
         // Creates a variable to store just the hour.
         int hour = currentTime.getHour();
         // If statement to make the hour up to 12 characters instead of 24.  I will change this back to 24 for the actual program.
-//        if (currentTime.getHour() > 12) {
+//        if (currentTime.getHour() > 10) {
 //            hour = currentTime.getHour() - 12;
 //        }
         // Creates a variable to store just the minute.
         int minute = currentTime.getMinute();
-        // Displays the current time of the message so the encrypted message can be decrypted.
-        System.out.println(hour + ":" + minute);
+        // Displays the a selector code for the decripter to use to determine the original time.
+        System.out.print(hour);
+        System.out.println(minute);
         // Creates an array to select either the hour or the minute.
         int[] selector = {1, 2};
         // For loop to print the message and the random characters.
---
+++ out/production/Rafagithub135.github.io/js/functions.js
@@ -0,0 +1,62 @@
+"use strict";
+
+    function calculateAge() {
+        let today = new Date();
+        let workStart = new Date("April 10, 1983");
+        let age = today.getFullYear() - workStart.getFullYear();
+        let m = today.getMonth() - workStart.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < workStart.getDate())) {
+            age--;
+        }
+        return age;
+    }
+
+    calculateAge(new Date());
+
+    (() => {
+
+    const skills = ['HTML', 'Java', 'Ajax', 'Python']
+
+    const skills2 = ['CSS', 'Bootstrap', 'Jquery'];
+
+    const skills3 = ['JavaScript', 'Spring Boot', 'Computer Network Wiring'];
+
+    for (let skill of skills) {
+        document.querySelector("#skills-list1").innerHTML += `<li>${skill}</li>`;
+    }
+
+    for (let skill of skills2) {
+        document.querySelector("#skills-list2").innerHTML += `<li>${skill}</li>`;
+    }
+
+    for (let skill of skills3) {
+        document.querySelector("#skills-list3").innerHTML += `<li>${skill}</li>`;
+}
+    var write = document.getElementById('write');
+    write.addEventListener('click', encodeInput);
+    function encodeInput() {
+        let statement = prompt('Enter the message you would like to encode.');
+        let phrase = '';
+        let randomCharacter = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'];
+        let keycodeCharacter = ['h', 'm', 's'];
+        for (var i = 0; i < keycodeCharacter.length; i += 1) {
+            if (keycodeCharacter[i] === 'h') {
+                keycodeCharacter[i] = '10';
+            } else if (keycodeCharacter[i] === 'm') {
+                keycodeCharacter[i] = '2';
+            } else {
+                keycodeCharacter[i] = '5';
+            }
+        }
+        let numberOfCharacters = keycodeCharacter[Math.floor(Math.random() * keycodeCharacter.length)];
+        for (var i = 0; i < statement.length; i += 1) {
+            phrase += statement[i];
+            if (i !== statement.length - 1) {
+                for (var j = 0; j < numberOfCharacters; j += 1) {
+                    phrase += randomCharacter[Math.floor(Math.random() * randomCharacter.length)];
+                }
+            }
+        }
+    }
+        return phrase;
+})();
